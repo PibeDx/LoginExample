31f4a210ae93b7e80ddbf6b327a08399c063250c
José Cuentas <jcuentast@gmail.com>
2016-08-02 11:18:07 -0500

    Add Contants keys

---
+++ app/src/main/java/com/josecuentas/loginexample/utils/Contants.java
@@ -0,0 +1,11 @@
+package com.josecuentas.loginexample.utils;
+
+/**
+ * Created by José Norberto Cuentas Turpo.
+ * Email: <jcuentast@gmail.com> on 2/08/16.
+ * GitHub: PibeDx
+ */
+public class Contants {
+	public static final String APPLICATION_KEY = "YOUR_KEY_APPLICATION";
+	public static final String REST_API_KEY = "YOUR_KEY_API";
+}
